46f8fc616f4b2af626daae73b8580931c7e604f4
Luis Nieto <luisnieto16@outlook.es>
2020-01-11 10:50:43 +0100

    feat(#11): Generar carrusel dinámico

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -24,28 +24,106 @@ $(function () {
             categorias = respuestaCategoria[0];
             imagenesCarrusel = respuestaCarrusel[0];
 
+            let categoriasReady = false;
+            let imagesReady = false;
+
             if (categorias.status >= 200 && categorias.status <= 299) {
-                cargarCartas();
+                // cargarCartas();
+                // cargarPaginaInicio();
+                categoriasReady = true;
                 pintarItemsSidebar();
             } else {
                 mostrarErrorDatos(categorias);
             }
 
             if (imagenesCarrusel.status >= 200 && imagenesCarrusel.status <= 299) {
-                getImgCarrusel();
+                imagesReady = true;
+                // getImgCarrusel();
             } else {
                 mostrarErrorDatos(imagenesCarrusel);
             }
 
+            if ( imagesReady && categoriasReady) {
+                cargarPaginaInicio();
+            }
+
             setListenersMenu();
-            informacion();
-            volver();
         })
         .fail(function (xhr) {
             console.log("ERROR del servidor: " + xhr.status + "(" + xhr.statusText + ")");
             console.log("Más información: ", xhr);
         });
 })
+
+function cargarPaginaInicio() {
+    $('#contenido').empty();
+    let lColumns = $('<div/>').addClass('l-columns').attr('id','l-columns-contenido');
+    $('#contenido').append(lColumns);
+    cargarCarousel();
+    cargarCartasSection();
+    cargarAsistente();
+    informacion();
+    volver();
+}
+
+function cargarCarousel() {
+    let lColumns = $('#l-columns-contenido');
+    let lColumnsArea = $('<div/>').addClass('l-columns__area');
+    let section = $('<div/>').addClass('c-section c-section--light c-section--padding-vertical-xl c-section--padding-horizontal-xs');        
+    let sectionContent = $('<div/>').addClass('c-section__content');
+    let carouselContainer = $('<div/>').addClass('carousel slide').attr({
+        id:'carouselExampleCaptions',
+        'data-ride':'carousel'
+    });
+    let ol = $('<ol/>').addClass('carousel-indicators');
+    let carouselInner = $('<div/>').addClass('carousel-inner');
+    let aPrev = $('<a/>').addClass('carousel-control-prev').attr({
+        href: '#carouselExampleCaptions',
+        role: 'button',
+        'data-slide': 'prev'
+    });
+    let spanPrevIcon = $('<span/>').addClass('carousel-control-prev-icon').attr('aria-hidden', 'true');
+    let spanPrevOnly = $('<span/>').addClass('sr-only').text('Previous');
+    let aNext = $('<a/>').addClass('carousel-control-next').attr({
+        href: '#carouselExampleCaptions',
+        role: 'button',
+        'data-slide': 'next'
+    });
+    let spanNextIcon = $('<span/>').addClass('carousel-control-next-icon').attr('aria-hidden', 'true');
+    let spanNextOnly = $('<span/>').addClass('sr-only').text('Next');
+
+    aPrev.append(spanPrevOnly);
+    aPrev.append(spanPrevIcon);
+    aNext.append(spanNextIcon);
+    aNext.append(spanNextOnly);
+    carouselContainer.append(aNext);
+    carouselContainer.append(aPrev);
+    carouselContainer.append(carouselInner);
+    carouselContainer.append(ol);
+    sectionContent.append(carouselContainer);
+    section.append(sectionContent);
+    lColumnsArea.append(section);
+    lColumns.append(lColumnsArea)
+
+    getImgCarrusel();
+}
+
+function cargarCartasSection() {
+    let lColumns = $('#l-columns-contenido');
+    let lColumnsArea = $('<div/>').addClass('l-columns__area');
+    let section = $('<div/>').addClass('c-section c-section--light c-section--padding-vertical-xxl c-section--padding-horizontal-m');
+    let sectionTitle = $('<h1/>').addClass('c-section__title').text('Categorías');
+    let sectionContent = $('<div/>').addClass('c-section__content');
+    let lColumnsCartas = $('<div/>').addClass('l-columns l-columns--3-columns').attr('id', 'cartas');
+
+    section.append(sectionTitle);
+    sectionContent.append(lColumnsCartas);
+    section.append(sectionContent);
+    lColumnsArea.append(section);
+    lColumns.append(lColumnsArea);
+
+    cargarCartas();
+}
 
 function cargarCartas() {
 
@@ -80,6 +158,31 @@ function cargarCartas() {
 
 }
 
+function cargarAsistente(){
+    let lColumns = $('#l-columns-contenido');
+    let lColumnsArea = $('<div/>').addClass('l-columns__area');
+    let section = $('<div/>').addClass('c-section');
+    let sectionContent = $('<div/>').addClass('c-section__content');
+    let asistente = $('<div/>').addClass('c-asistente').attr('style', "background-image: url('./assets/img/fondo-asistente.jpg')");
+    let asistenteCabecera = $('<h2/>').addClass('c-asistente__cabecera').text('¿Necesitas ayuda?');
+    let cabeceraIcon = $('<i/>').addClass('far fa-comment-alt');
+    let asistenteTexto = $('<p/>').addClass('c-asistente__texto').text('Llámanos al 636 876 856 y te atenderemos personalmente.');
+    let asistenteImagen = $('<img/>').addClass('c-asitente__imagen').attr('src', './assets/img/asistente.jpg');
+    let asistenteCerrar = $('<a/>').addClass('c-asistente__cerrar');
+    let cerrarIcon = $('<i/>').addClass('far fa-window-close');
+
+    asistenteCabecera.append(cabeceraIcon);
+    asistente.append(asistenteCabecera);
+    asistente.append(asistenteTexto);
+    asistente.append(asistenteImagen);
+    asistenteCerrar.append(cerrarIcon);
+    asistente.append(asistenteCerrar);
+    sectionContent.append(asistente);
+    section.append(sectionContent);
+    lColumnsArea.append(section);
+    lColumns.append(lColumnsArea);
+}
+
 function setListenerCartasBoton(cardBoton) {
     cardBoton.on('click', function () {
         alert('En un sprint futuro podrás ver más información de: ' + $(this).attr('id'));
@@ -105,6 +208,7 @@ function pintarItemsSidebar() {
 function setListenersItemsSidebar() {
     // Listener en el menú que afectará a todos los items (habidos y nuevos)
     $("#c-sidebar").on("click", ".c-sidebar__item", function () {
+        $('.c-menu__option--selected').removeClass('c-menu__option--selected');
         let idCategoria = this.id.split('-');
         mostrarServicios(idCategoria[1]);
     })
@@ -125,11 +229,11 @@ function mostrarServicios(idCategoria) {
         let lColumnsInsideSection = $('<div/>').addClass('l-columns l-columns--4-columns').attr('id', 'contenedor-articulos');
         $.each(response.data, function(index, value){
             let lColumnsInsideSectionArea = $('<div/>').addClass('l-columns__area');
-            let articulo = $('<div/>').addClass('articulo');
-            let articuloImg = $('<img/>').addClass('articulo__image').attr('src','./assets/img/img1.jpg');
-            let articuloTitulo = $('<h3/>').addClass('articulo__title').text(value.nombre);
-            let articuloPrecio = $('<h6/>').addClass('articulo__price').text(value.precio+'€');
-            let articuloBoton = $('<a/>').addClass('articulo__button').text('Ver en detalle');
+            let articulo = $('<div/>').addClass('c-articulo');
+            let articuloImg = $('<img/>').addClass('c-articulo__image').attr('src','./assets/img/img1.jpg');
+            let articuloTitulo = $('<h3/>').addClass('c-articulo__title').text(value.nombre);
+            let articuloPrecio = $('<h6/>').addClass('c-articulo__price').text(value.precio+'€');
+            let articuloBoton = $('<a/>').addClass('c-articulo__button').text('Ver en detalle');
 
             articulo.append(articuloImg);
             articulo.append(articuloTitulo);
@@ -151,7 +255,7 @@ function mostrarServicios(idCategoria) {
 }
 
 function addListenersServicios(){
-    $(".articulo").on("click", ".articulo__button", function(){
+    $(".c-articulo").on("click", ".c-articulo__button", function(){
         alert('Mostrar modal con la información');
     });
 }
@@ -192,4 +296,15 @@ function getImgCarrusel() {
 
 function setListenersMenu() {
     menu();
+    setListenersButtonsMenu();
+}
+
+function setListenersButtonsMenu() {
+    $('.c-menu').on('click', '.c-menu__option', function(){
+        let id = $(this).attr('id');
+
+        if ( id === 'menu-inicio' ) {
+            cargarPaginaInicio();
+        }
+    })
 }
